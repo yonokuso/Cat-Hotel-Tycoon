90f514c73cca9c46d64b3a02daf14fcfa861571d
yonokuso <qltenwkd@naver.com>
2023-12-18 17:26:27 +0900

    coinmove

--- Assets/_Project/Scripts/CoinMove.cs
+++ Assets/_Project/Scripts/CoinMove.cs
@@ -14,13 +14,13 @@ public class CoinMove : MonoBehaviour
         if (collision != null)
         {
             Debug.Log("¹Ù´Ú °¨Áö");
-            coin.transform.DOJump(new Vector3(2, 0), 200f, 2, 2f);
+            coin.transform.DOJump(new Vector3(2, 0), 2f, 2, 2f);
             // DOJump(Vector3 to, float jumpPower, int numJumps, float duration, bool snapping(true))
         }
 
         SoundManager.instance.PlaySE(coin_Sound2);
 
-        Invoke("coinActive", 2);
+        Invoke("coinActive", 3);
 
     }
 
